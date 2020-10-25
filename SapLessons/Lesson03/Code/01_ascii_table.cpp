917567171d78706a59bd943504d9bc39e4becb80
Malov Vladimir <vldmalov@gmail.com>
2020-10-25 20:40:55 +0300

    Miit. Ascii table base ostream output

--- SapLessons/Lesson03/Code/01_ascii_table.cpp
+++ SapLessons/Lesson03/Code/01_ascii_table.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+//#include <fstream>
 
 // Форматирование
 // Перегрузка функций
 // Вынос констант
+// Вывод в файл
+// Вывод в файл/на консоль. Перегрузка
+// Вывод в строку
 
 // const int OUTPUT_FIELD_WIDTH = 7;
 
-void PrintAsciiTable(unsigned char firstSymbol, unsigned char lastSymbol)
+void PrintAsciiTable(/*std::ostream& os,*/ unsigned char firstSymbol, unsigned char lastSymbol)
 {
     if(firstSymbol > lastSymbol) {
         std::cerr << "First symbol code " << (int)firstSymbol << " more than last one " << (int)lastSymbol << std::endl;
@@ -19,8 +23,9 @@ void PrintAsciiTable(unsigned char firstSymbol, unsigned char lastSymbol)
     std::cout.width(7); std::cout << "Char" << std::endl;
 
     do {
-        std::cout.width(7); std::cout << std::dec << (int)firstSymbol;
-        std::cout.width(7); std::cout << std::showbase << std::hex << (int)firstSymbol;
+        const int symbolCode = static_cast<int>(firstSymbol);
+        std::cout.width(7); std::cout << std::dec << symbolCode;
+        std::cout.width(7); std::cout << std::hex << std::showbase << symbolCode;
         std::cout.width(7); std::cout << firstSymbol << std::endl;
 
         ++firstSymbol;
@@ -42,4 +47,7 @@ int main()
     // PrintAsciiTable('A', 'z');
     // PrintAsciiTable(0, 128);
     // PrintAsciiTable(128, 255);
+
+    //std::ofstream ofs("AsciiTable.txt");
+    //PrintAsciiTable(ofs, 'a', 'z');
 }
